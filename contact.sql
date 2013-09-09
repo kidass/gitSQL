1ec2b74c694b46f63706ff7641ca83cd623c51d6
kidass <kidass@163.com>
2013-09-09 17:46:25 +0800

    update staffid

--- contact.sql
+++ contact.sql
@@ -9,10 +9,12 @@ CREATE TABLE [dbo].[Customer_B_ContactRecord](
 	[RecordID] [int] IDENTITY(1,1) NOT NULL,
 	[CustomerID] int  not NULL,
 	[StaffID] [varchar](50) not NULL,
+	[StaffName] [varchar](50) not NULL,	
 	[ContactDate] [datetime] NULL,
 	[ContactRecord] [varchar](1500)  NULL DEFAULT (''),
 	[NextContactDate] [datetime] NULL,
 	[NextContactRecord] [varchar](1500)  NULL DEFAULT (''),
+	[CreatDate] [datetime] NULL,
  CONSTRAINT [PK_Customer_B_ContactRecord] PRIMARY KEY CLUSTERED 
 (
 	[RecordID] ASC
@@ -21,8 +23,9 @@ CREATE TABLE [dbo].[Customer_B_ContactRecord](
 
 --客户基本表：ID，姓名，标签，公司ID,公司名称，部门，电话，地址，背景，
 CREATE TABLE [dbo].[Customer_B_Info](
-	[InforID] [int] IDENTITY(1,1) NOT NULL,
+	[CustomerID] [int] IDENTITY(1,1) NOT NULL,
 	[CustomerName] [varchar](50)  NULL DEFAULT (''),
+	[StaffID] [varchar](50) not NULL,
 	[TagTitle] [varchar](50)  NULL DEFAULT (''),
 	[CompanyID] int  NULL,
 	[CompanyName] [varchar](100)  NULL DEFAULT (''),
@@ -30,9 +33,10 @@ CREATE TABLE [dbo].[Customer_B_Info](
 	[PhoneNumber] [varchar](100)  NULL DEFAULT (''),
 	[Address] [varchar](100)  NULL DEFAULT (''),
 	[BackGroundInfo] [varchar](1000)  NULL DEFAULT (''),
+	[CreatDate] [datetime] NULL,
  CONSTRAINT [PK_Customer_B_Info] PRIMARY KEY CLUSTERED 
 (
-	[InforID] ASC
+	[CustomerID] ASC
 )WITH (IGNORE_DUP_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 
@@ -40,6 +44,7 @@ CREATE TABLE [dbo].[Customer_B_Info](
 CREATE TABLE [dbo].[Customer_B_Company](
 	[CompanyID] [int] IDENTITY(1,1) NOT NULL,
 	[CompanyName][varchar](50) not NULL,
+	[CreatDate] [datetime] NULL,
  CONSTRAINT [PK_Customer_B_Company] PRIMARY KEY CLUSTERED 
 (
 	[CompanyID] ASC
@@ -49,7 +54,7 @@ CREATE TABLE [dbo].[Customer_B_Company](
 --公司信息表：公司ID，公司信息,创建日期
 CREATE TABLE [dbo].[Customer_B_CompanyInfo](
 	[CompanyID] [int]  NOT NULL,
-	[CompanyNameInfo][varchar](1500) not NULL,
+	[CompanyInfo][varchar](1500) not NULL,
 	[CreatDate] [datetime] NULL,
  CONSTRAINT [PK_Customer_B_CompanyInfo] PRIMARY KEY CLUSTERED 
 (
@@ -60,9 +65,11 @@ CREATE TABLE [dbo].[Customer_B_CompanyInfo](
 --标签表：ID,公司ID，客户ID，标签名称
 CREATE TABLE [dbo].[Customer_B_Tag](
 	[TagID] [int] IDENTITY(1,1) NOT NULL,
+	[StaffID] [varchar](50) not NULL,
 	[CompanyID] [int]   NULL,
 	[CustomerID]int NULL,
 	[TagName] varchar(50) NULL default(''),
+	[CreatDate] [datetime] NULL,
  CONSTRAINT [PK_Customer_B_Tag] PRIMARY KEY CLUSTERED 
 (
 	[TagID] ASC
@@ -72,12 +79,15 @@ CREATE TABLE [dbo].[Customer_B_Tag](
 --交易表：ID，公司ID，客户ID,名字，描述，金额，类别
 CREATE TABLE [dbo].[Customer_B_Deal](
 	[DealID] [int] IDENTITY(1,1) NOT NULL,
+	[StaffID] [varchar](50) not NULL,
 	[CompanyID] [int]   NULL,
 	[CustomerID]int NULL,
 	[DealName] varchar(50) NULL default(''),
 	[DealInfo] varchar(1500) NULL default(''),
 	[DealMoney] [numeric](16, 2) NULL DEFAULT (0),
 	[DealCategory] varchar(50) NULL default(''),
+	[DealDate] [datetime] NULL,
+	[CreatDate] [datetime] NULL,
  CONSTRAINT [PK_Customer_B_Deal] PRIMARY KEY CLUSTERED 
 (
 	[DealID] ASC
